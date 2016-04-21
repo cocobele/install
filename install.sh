42631504097d84759a96a288c6df8885fbe5df58
cocobele <124155148@qq.com>
2016-04-21 15:46:36 +0800

    yum

--- install.sh
+++ install.sh
@@ -19,8 +19,33 @@ EOF
 echo -e "++++++++++++++++++++++++++++Section 0 Ready++++++++++++++++++++++++++++"
 
 . ./ready.conf
+#检查是否有root权限
+root
 #Require root
 disable_selinux
+## 停止不必要的服务
 stop_servers
+##屏蔽交互提示
 mask_cross
+### 在root用户目录建立.ssh目录便于远程上传文件更新
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+
+### 卸载可能的邮件服务，避免冲突
+yum -y remove sendmail &>/dev/null
+yum -y remove postfix &>/dev/null
+
+### 下载需要的源代码
 DOWNLOAD
+
+##如系购买美国VPS主机，则时间可能不对需要，调整为上海时间，同时要停下检查系统时间对不对
+if date | grep CST &>/dev/null; then
+    echo ''
+    else
+        rm -rf /etc/localtime
+        ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+fi
+
+## first install compile package  如为CentOS
+requirerpm
+
