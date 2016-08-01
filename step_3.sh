53a9f6434966ad98209b3d6cb83df06db323ee29
cocobele <124155148@qq.com>
2016-08-01 16:31:37 +0800

    sad

--- step_3.sh
+++ step_3.sh
@@ -1,3 +1,13 @@
 ##################################################################################  apache.conf
 . ./conf/apache.conf
 apache_install
+php_install
+ucspi-tcp_install
+php-json_install
+zlib_install
+qmail_install
+vpopmail_install
+qmail_config
+daemontools_install
+qmail_setup
+ezmlm
